be2fd40c6b378e92ae291c49829e2ff91e9b5616
alana@digitalhouse.com <alana@digitalhouse.com>
2021-03-27 14:40:39 +0000

    Mumuki Export on 2021-03-27 14:40:39 +0000

--- 00003_Função com parâmetro opcional/test.py
+++ 00003_Função com parâmetro opcional/test.py
@@ -5,3 +5,13 @@ class Test(unittest.TestCase):
 
   def test_deve repetir_5_vezes_por_padrao(self):
     self.assertEqual(repete('Aa'), 'AaAaAaAaAa')
+    
+  def test_deve repetir_3_vezes_por_padrao(self):
+    self.assertEqual(repete('Olá! ', 3), 'Olá! Olá! Olá! ')
+    
+  def test_deve repetir_3_vezes_por_padrao(self):
+    self.assertEqual(repete('Olá! '), 'Olá! Olá! Olá! Olá! Olá! ')
+    
+    
+    
+  
